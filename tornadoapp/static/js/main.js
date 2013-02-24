a243dd41ee13cc9844a9766cf6ba4448e8b5c57e
ashoaib <abu.rm.shoaib@gmail.com>
2013-02-24 21:52:18 +0000

    Changed active link styles; modified form layout

--- tornadoapp/static/js/main.js
+++ tornadoapp/static/js/main.js
@@ -2,12 +2,15 @@ var contact = {
 	defaults: {},
 	
 	init: function() {
+		var name = $('#name');
 		var email = $('#email');
 		var message = $('#message');
 		
+		contact.defaults['name'] = name.val();
 		contact.defaults['email'] = email.val();
 		contact.defaults['message'] = message.val();
 		
+		name.on('focus', contact.onFocus).on('blur', contact.onBlur);
 		email.on('focus', contact.onFocus).on('blur', contact.onBlur);
 		message.on('focus', contact.onFocus).on('blur', contact.onBlur);		
 	},
