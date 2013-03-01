e67edbaec630b719c3f50544343c6c566380090e
ashoaib <abu.rm.shoaib@gmail.com>
2013-03-01 21:32:40 +0000

    Changed ConfigParser to SafeConfigParser; changed path_to_conf to conf_path

--- tornadoapp/mongodb.py
+++ tornadoapp/mongodb.py
@@ -8,8 +8,8 @@ This is a wrapper class for pymongo.
 - Has methods like get, put to wrap around pymongo functions
 """
 class MongoDB:
-    def __init__(self, path_to_conf):
-        self._load_config(path_to_conf)
+    def __init__(self, conf_path):
+        self._load_config(conf_path)
         self.conn = pymongo.MongoClient(self.config['hostname'], self.config['port'])
         self.db = self.conn[self.config['dbname']]
         self.db.authenticate(self.config['username'], self.config['password'])
@@ -20,17 +20,17 @@ class MongoDB:
     def get(self, collection, search_dict=dict()):
         return self.db[collection].find(search_dict)
         
-    def _load_config(self, path_to_conf):
+    def _load_config(self, conf_path):
         conf_keys = ['hostname', 'port', 'username', 'password', 'dbname']
         
         try:
-            config = ConfigParser.ConfigParser()
-            config.read(path_to_conf)
+            config = ConfigParser.SafeConfigParser()
+            config.read(conf_path)
             self.config = {key: config.get('main', key) for key in conf_keys}
         except ConfigParser.NoOptionError:
             raise KeyError('Config options not set correctly')
         except ConfigParser.Error:
-            raise IOError('Config file not found at ' + path_to_conf)
+            raise IOError('Config file not found at ' + conf_path)
         
 #        with open(path_to_conf) as conf_file:
 #            self._config = {key: value for (key, value) in conf_file.readlines().split('=')}
