517d5f4d918da98562ba7c44e209fb0ea07069a2
ashoaib <abu.rm.shoaib@gmail.com>
2013-03-01 23:03:15 +0000

    Updated mongodb.py with empty CRUD methods

--- tornadoapp/mongodb.py
+++ tornadoapp/mongodb.py
@@ -10,18 +10,38 @@ This is a wrapper class for pymongo.
 class MongoDB:
     def __init__(self, conf_path):
         self._load_config(conf_path)
-        self.conn = pymongo.MongoClient(self.config['hostname'], self.config['port'])
-        self.db = self.conn[self.config['dbname']]
-        self.db.authenticate(self.config['username'], self.config['password'])
+        
+        try:
+            self.conn = pymongo.MongoClient(self.config['hostname'], int(self.config['port']))
+            self.db = self.conn[self.config['dbname']]
+            
+            if all(k in self.config for k in ['username', 'password']):
+                self._authenticate()
+            else:
+                print "Warning: mongodb not authenticated on " + str(self.config['dbname'])
+        except pymongo.errors.PyMongoError:
+            raise Exception('MongoDB connection failed!')
         
     def use(self, db_name):
         self.db = self.conn[db_name]
         
-    def get(self, collection, search_dict=dict()):
-        return self.db[collection].find(search_dict)
+    def get(self, collection, query=dict()):
+        return self.db[collection].find(query)
+    
+    def put(self, collection, data=None):
+        pass
+    
+    def update(self, collection, query=dict(), data=None):
+        pass
+    
+    def delete(self, collection, query=dict()):
+        pass
+    
+    def _authenticate(self):
+        self.db.authenticate(self.config['username'], self.config['password'])
         
     def _load_config(self, conf_path):
-        conf_keys = ['hostname', 'port', 'username', 'password', 'dbname']
+        conf_keys = ['hostname', 'port', 'dbname']
         
         try:
             config = ConfigParser.SafeConfigParser()
