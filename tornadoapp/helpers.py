571f90d3ca59da409aad6a0b40d5fce4d78fbce1
ashoaib <abu.rm.shoaib@gmail.com>
2013-03-31 13:06:09 +0100

    Added Password class to helpers.py

--- tornadoapp/helpers.py
+++ tornadoapp/helpers.py
@@ -1,2 +1,12 @@
-def hash_password(password):
-    return password
+import bcrypt
+
+class Password:
+    """Wrapper class for hashing and comparing passwords using bcrypt"""
+    
+    @staticmethod
+    def hash(password):
+        return bcrypt.hashpw(password, bcrypt.gensalt())
+    
+    @staticmethod
+    def compare(password, hashed):
+        return bcrypt.hashpw(password, hashed) == hashed
